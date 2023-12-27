cec9427665e7ca63e18c8557b36571d09fb1b30b
Stefan Misik <stefan-misik@users.noreply.github.com>
2023-12-27 18:57:02 +0100

    chore: add EEPROM buffer tool

--- fw/nvm_storage.cpp
+++ fw/nvm_storage.cpp
@@ -1,8 +1,69 @@
 #include "nvm_storage.h"
 
-#include "util/crc16.h"
+#include <util/crc16.h>
+#include <avr/eeprom.h>
+
 
 namespace {
 
+}  // namespace
+
+
+bool EepromBuffer::begin(uint8_t length, uintptr_t address)
+{
+    {
+        if (length > BUFFER_CAPACITY)
+            return false;
+
+        if (address >= EEPROM_SIZE)
+            return false;
+
+        const uintptr_t end = address + length;
+        if (end > EEPROM_SIZE)
+            return false;
+    }
+
+    address_ = address;
+    length_ = length;
+    position_ = 0;
+    return true;
 }
+
+bool EepromBuffer::continueWrite()
+{
+    if (length_ == position_)
+        return true;
+
+    // Wait for previous write to complete
+    if (!eeprom_is_ready())
+        return false;
+
+    // Initiate single byte write
+    EEAR = (address_ + position_);
+    EEDR = buffer_[position_];
+    EECR |= (1 << EEMPE);
+    EECR |= (1 << EEPE);
+    ++position_;
+
+    return false;
+}
+
+bool EepromBuffer::continueRead()
+{
+    if (length_ == position_)
+        return true;
+
+    // EEPROM can not be read from, before previous write completes
+    if (!eeprom_is_ready())
+        return false;
+
+    // Perform single byte read
+    EEAR = (address_ + position_);
+    EECR |= (1 << EERE);
+    buffer_[position_] = EEDR;
+    ++position_;
+
+    return false;
+}
+
 
--- fw/nvm_storage.h
+++ fw/nvm_storage.h
@@ -5,21 +5,69 @@
 #ifndef NVM_STORAGE_H_
 #define NVM_STORAGE_H_
 
-#include <stddef.h>
+#include <stdint.h>
 
 
 struct AnimationConfigurationData
 {
-    static const size_t LENGTH = 7;
+    static const uint8_t LENGTH = 4;
     char data[LENGTH];
 };
 
 
-class NvmStorage
+/**
+ * @brief Tool used to access data located in EEPROM
+ */
+class EepromBuffer
 {
 public:
-private:
+    static const uint8_t BUFFER_CAPACITY = 8;
 
+    static const uintptr_t EEPROM_SIZE = 512;
+
+    EepromBuffer():
+        length_(0),
+        position_(0)
+    { }
+
+    bool isDone() const { return length_ == position_; }
+
+    /**
+     * @brief Begin writing/writing data in the buffer
+     *
+     * @param length Length of the new operation
+     * @param address EEPROM address to write into / read from
+     *
+     * @return Operation successfully started
+     */
+    bool begin(uint8_t length, uintptr_t address);
+
+    /**
+     * @brief Continue writing
+     *
+     * @return Is finished
+     */
+    bool continueWrite();
+
+    /**
+     * @brief Continue writing
+     *
+     * @return Is finished
+     */
+    bool continueRead();
+
+    uint8_t capacity() const { return BUFFER_CAPACITY; }
+
+    void * buffer() { return reinterpret_cast<void *>(buffer_); }
+
+    const void * buffer() const { return reinterpret_cast<const void *>(buffer_); }
+    uint8_t lebgth() const { return length_; }
+
+private:
+    uintptr_t address_ = 0;
+    char buffer_[BUFFER_CAPACITY];
+    uint8_t length_;
+    uint8_t position_;
 };
 
 
