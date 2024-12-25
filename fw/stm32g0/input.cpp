af6fd539aa1b1f4c86857ad8ecec82a4cc5b68ce
Stefan Misik <stefan-misik@users.noreply.github.com>
2024-12-25 14:44:11 +0100

    chore(fw): implement input source read-out

--- fw/stm32g0/input.cpp
+++ fw/stm32g0/input.cpp
@@ -5,3 +5,14 @@ namespace
 {
 
 }  // namespace
+
+
+void Input::update(std::uint32_t time)
+{
+    ButtonStateList list;
+    for (std::size_t src_id = 0; src_id != SOURCE_COUNT; ++src_id)
+    {
+        list.resetAll();
+        sources_[src_id]->getPressedKeys(time, &list);
+    }
+}
--- fw/stm32g0/input.hpp
+++ fw/stm32g0/input.hpp
@@ -7,10 +7,21 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <utility>
 
 #include "tools/polymorphic_storage.hpp"
 
 
+template <typename T>
+inline constexpr std::size_t calculateBitArrayLength(std::size_t length)
+{
+    const std::size_t div = length / (sizeof(T) * 8);
+    return (div * (sizeof(T) * 8)) != length ? div + 1 : div;
+}
+
+/**
+ * @brief Key-input object
+ */
 class Input
 {
 public:
@@ -26,6 +37,58 @@ public:
         KEY_COUNT_
     };
 
+    static const inline std::size_t KEY_COUNT = static_cast<std::size_t>(KeyId::KEY_COUNT_);
+
+    /**
+     * @brief Type used to represent a list of pressed buttons
+     */
+    class ButtonStateList
+    {
+    private:
+        using Type = std::uint32_t;
+
+        static const std::size_t ARRAY_SIZE = calculateBitArrayLength<Type>(KEY_COUNT);
+
+        static std::pair<std::size_t, Type> makeOffsetAndMask(std::size_t bit)
+        {
+            const std::size_t offset = bit / (sizeof(Type) * 8);
+            const std::size_t bit_within = bit - (offset * (sizeof(Type) * 8));
+            return {offset, static_cast<Type>(1) << bit_within};
+        }
+
+    public:
+        void resetAll()
+        {
+            for (auto & d : data_)
+                d = 0U;
+        }
+
+        void setKey(KeyId key)
+        {
+            const auto [offset, mask] = makeOffsetAndMask(static_cast<std::size_t>(key));
+            data_[offset] |= mask;
+        }
+
+        void resetKey(KeyId key)
+        {
+            const auto [offset, mask] = makeOffsetAndMask(static_cast<std::size_t>(key));
+            data_[offset] &= ~mask;
+        }
+
+        bool isKeySet(KeyId key) const
+        {
+            const auto [offset, mask] = makeOffsetAndMask(static_cast<std::size_t>(key));
+            return 0 != (data_[offset] & mask);
+        }
+
+    private:
+        Type data_[ARRAY_SIZE];
+    };
+
+
+    /**
+     * @brief Type representing state of single button
+     */
     class KeyState
     {
     public:
@@ -33,17 +96,36 @@ public:
         std::uint32_t state_ = 0;
     };
 
+    /**
+     * @brief Interface for input sources
+     */
     class Source
     {
     public:
-        virtual void process() = 0;
+        /**
+         * @brief Function called periodically to obtain list of pressed keys
+         *
+         * @param time Current time
+         * @param[out] buttons List which will obtain pressed keys, presume all keys are initially cleared
+         */
+        virtual void getPressedKeys(std::uint32_t time, ButtonStateList * buttons) = 0;
+
         virtual ~Source() = default;
     };
 
-    static const inline std::size_t KEY_COUNT = static_cast<std::size_t>(KeyId::KEY_COUNT_);
+    static const inline std::size_t SOURCE_COUNT = 2;
+
+    /**
+     * @brief Call periodically to obtain list of pressed keys
+     *
+     * @param time Current time
+     */
+    void update(std::uint32_t time);
 
 private:
     using SourceStorage = PolymorphicStorage<Source, 16>;
+
+    SourceStorage sources_[SOURCE_COUNT];
 
     KeyState keys_[KEY_COUNT];
 };
